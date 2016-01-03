87951b26766addf1a3174970993f2d0161b59454
Jay Dihenkar <jkdihenkar@gmail.com>
2016-01-03 16:06:34 +0530

    Adding 'if' 'case' and recursion example

--- src/hello.erl
+++ src/hello.erl
@@ -23,7 +23,35 @@ say_hi() ->
 add(A,B) ->
 	A+B.
 
+%%If Construct
+check_if(A) ->
+	if
+		is_atom(A) -> "Atom";
+		is_boolean(A) -> "Boolean";
+		is_integer(A) -> "Integer";
+		is_tuple(A) -> "Tuple/String";
+		is_list(A) -> "List"
+	end.
 
+%%Checking case construct -- case ex 1
+check_case(B) ->
+	case B of 
+		"1" -> 1;
+		"2" -> 2;
+		"3" -> 3;
+		Other -> "Not expected Number!"
+	end.
+
+%%even odd logic
+check_evenodd(A) ->
+	case (A rem 2) of
+		0 -> "Even";
+		1 -> "Odd"
+	end.
+
+%%recursive fact 
+factorial(0) -> 1;
+factorial(N) -> N * factorial(N-1).
 
 
 
