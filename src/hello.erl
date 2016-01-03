e7af53b9b241601d2865847f7feaad6d4a96ee39
Jay Dihenkar <jkdihenkar@gmail.com>
2016-01-03 13:58:24 +0530

    Console OP and Simple Arith in module construct

---
+++ src/hello.erl
@@ -0,0 +1,32 @@
+%% @author jkdihenkar
+%% @doc @todo Add description to hello.
+
+
+-module(hello).
+
+%% ====================================================================
+%% API functions
+%% ====================================================================
+-compile(export_all).
+
+
+
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
+
+%% Saying hello world the erlang way
+say_hi() ->
+	io:format("Hello world...!!").
+
+%% Add two numbers
+add(A,B) ->
+	A+B.
+
+
+
+
+
+
+
+
